a26bdc0cf04b79bc04bc3df6b4a411c53252aafc
emanameen9982773 <emanothard15@gmail.com>
2026-05-06 01:20:58 +0300

    render_template for all pages

--- app.py
+++ app.py
@@ -7,6 +7,33 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
+@app.route('/login.html')
+def login():
+    return render_template('login.html')
+
+@app.route('/secure_login.html')
+def secure_login():
+    return render_template('secure_login.html')
+
+@app.route('/register.html')
+def register():
+    return render_template('register.html')
+
+@app.route('/secure_register.html')
+def register():
+    return render_template('secure_register.html')
+
+@app.route('/dashboard.html')
+def register():
+    return render_template('dashboard.html')
+
+@app.route('/admid.html')
+def register():
+    return render_template('admid.html')
+
+@app.route('/secure_admin.html')
+def register():
+    return render_template('secure_admin.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
