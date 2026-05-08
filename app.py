2b6faf7d1e082d42239ea901a79a8d49c83c7b1a
Leena <160266590+lollileeno@users.noreply.github.com>
2026-05-08 18:42:34 +0300

    Add make_response import to app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, flash, session
+from flask import Flask, render_template, request, redirect, url_for, flash, session , make_response
 import os
 import psycopg2
 import bcrypt  # For secure password storage
